67859be3ad3009d18a1553b533d32486dfc88374
Juanpa8830 <jp3_4@hotmail.com>
2022-11-23 17:27:19 -0500

    finish speakers dynamic section

--- app.js
+++ app.js
@@ -1,65 +1,64 @@
 // dinamyc cards
 
 const cards = [
-    {
-        id: 'card1',
-        name: 'Oliver L. Velez',
-        position: 'Co-founder of Pristine Capital Holdings, Inc.',
-        featImage: './images/oliverlogo.png',
-        description: `has been the subject of numerous articles and books written about Wall Street&apos;s
-        most successful traders, including popular books such as Bulls, Bears, and  Brains.`
-    },
+  {
+    id: 'card1',
+    name: 'Oliver L. Velez',
+    position: 'Co-founder of Pristine Capital Holdings, Inc.',
+    featImage: './images/oliverlogo.png',
+    description: `has been the subject of numerous articles and books written about Wall Street&apos;s
+        most successful traders, including popular books such as Bulls, Bears, and  Brains.`,
+  },
 
-    {
-        id: 'card2',
-        name: 'Oliver L. Velez',
-        position: 'Co-founder of Pristine Capital Holdings, Inc.',
-        featImage: './images/oliverlogo.png',
-        description: `has been the subject of numerous articles and books written about Wall Street&apos;s
-        most successful traders, including popular books such as Bulls, Bears, and  Brains.`
-    },
+  {
+    id: 'card2',
+    name: 'Marcello Arrambide',
+    position: 'Founder of Day Trading Academy.',
+    featImage: './images/marcellologo.PNG',
+    description: `has been the subject of numerous articles and books written about Wall Street&apos;s
+        most successful traders, including popular books such as Bulls, Bears, and  Brains.`,
+  },
 
-    {
-        id: 'card3',
-        name: 'Oliver L. Velez',
-        position: 'Co-founder of Pristine Capital Holdings, Inc.',
-        featImage: './images/oliverlogo.png',
-        description: `has been the subject of numerous articles and books written about Wall Street&apos;s
-        most successful traders, including popular books such as Bulls, Bears, and  Brains.`
-    },
+  {
+    id: 'card3',
+    name: 'Steve Cohen',
+    position: 'Co-founder of Pristine Capital Holdings, Inc.',
+    featImage: './images/steveCohen.png',
+    description: `His company was called S. A. C. Capital advisors and started with a capital of 20 million dollars.
+     Currently, Cohen is in the thirtieth place among the people with the most money in the United States.`,
+  },
 
-    {
-        id: 'card4',
-        name: 'Oliver L. Velez',
-        position: 'Co-founder of Pristine Capital Holdings, Inc.',
-        featImage: './images/oliverlogo.png',
-        description: `has been the subject of numerous articles and books written about Wall Street&apos;s
-        most successful traders, including popular books such as Bulls, Bears, and  Brains.`
-    },
+  {
+    id: 'card4',
+    name: 'David Tepper',
+    position: 'President and founder of Appaloosa Management.',
+    featImage: './images/DavidTepper.png',
+    description: `He also made several investments in which he generated profits, but it was not until 2009 that he won 
+    another quite considerable sum, which was close to 7 billion dollars.`,
+  },
 
-    {
-        id: 'card5',
-        name: 'Oliver L. Velez',
-        position: 'Co-founder of Pristine Capital Holdings, Inc.',
-        featImage: './images/oliverlogo.png',
-        description: `has been the subject of numerous articles and books written about Wall Street&apos;s
-        most successful traders, including popular books such as Bulls, Bears, and  Brains.`
-    },
+  {
+    id: 'card5',
+    name: 'Roberto A. Ruarte',
+    position: 'forerunner of Technical Analysis in Argentina and Latin America and founder of the company Ruarte Reports in 1991.',
+    featImage: './images/RobertoDuarte.png',
+    description: `Fresh and relevant financial instruments based on Elliott Wave Theory and Technical Analysis`,
+  },
 
-    {
-        id: 'card6',
-        name: 'Oliver L. Velez',
-        position: 'Co-founder of Pristine Capital Holdings, Inc.',
-        featImage: './images/oliverlogo.png',
-        description: `has been the subject of numerous articles and books written about Wall Street&apos;s
-        most successful traders, including popular books such as Bulls, Bears, and  Brains.`
-    },
+  {
+    id: 'card6',
+    name: 'John Bollinger',
+    position: 'Technical analyst and television commentator. Chairman and founder of Bollinger Capital Management Inc.',
+    featImage: './images/JohnBollinger.PNG',
+    description: `he is the creator of the "Bollinger Bands" and other related tools that have been integrated into most
+     graphics platforms and analytical software.`,
+  }
 ];
 
 const cardsList = document.getElementById('featcards');
 
 for (let i = 0; i < 2; i += 1) {
-    const cardMobileTemplate = `
+  const cardMobileTemplate = `
     
     <div class="${cards[i].id} showmobile">
 
@@ -74,11 +73,11 @@ for (let i = 0; i < 2; i += 1) {
 
     </div>`;
 
-    cardsList.innerHTML += cardMobileTemplate;
+  cardsList.innerHTML += cardMobileTemplate;
 }
 
 for (let i = 2; i < cards.length; i += 1) {
-    const cardMobileTemplate = `
+  const cardMobileTemplate = `
     
     <div class="${cards[i].id} hidemobile">
 
@@ -93,9 +92,8 @@ for (let i = 2; i < cards.length; i += 1) {
 
     </div>`;
 
-    cardsList.innerHTML += cardMobileTemplate;
+  cardsList.innerHTML += cardMobileTemplate;
 }
-
 
 // about section
 
@@ -113,41 +111,38 @@ const card5 = document.querySelector('.card5');
 const card6 = document.querySelector('.card6');
 
 hbutton.addEventListener('click', () => {
-    mobileMenu.classList.replace('hidemobile', 'showmobile');
-    contentMenu.classList.replace('showcontent', 'hidemobile');
+  mobileMenu.classList.replace('hidemobile', 'showmobile');
+  contentMenu.classList.replace('showcontent', 'hidemobile');
 });
 
 closeMenu.addEventListener('click', () => {
-    mobileMenu.classList.replace('showmobile', 'hidemobile');
-    contentMenu.classList.replace('hidemobile', 'showcontent');
+  mobileMenu.classList.replace('showmobile', 'hidemobile');
+  contentMenu.classList.replace('hidemobile', 'showcontent');
 });
 
 libutton1.addEventListener('click', () => {
-    mobileMenu.classList.replace('showmobile', 'hidemobile');
-    contentMenu.classList.replace('hidemobile', 'showcontent');
+  mobileMenu.classList.replace('showmobile', 'hidemobile');
+  contentMenu.classList.replace('hidemobile', 'showcontent');
 });
 
 libutton2.addEventListener('click', () => {
-    mobileMenu.classList.replace('show_menu', 'hide_menu');
+  mobileMenu.classList.replace('show_menu', 'hide_menu');
 });
 
 morebutton.addEventListener('click', () => {
-    card3.classList.replace('hidemobile', 'showmobile');
-    card4.classList.replace('hidemobile', 'showmobile');
-    card5.classList.replace('hidemobile', 'showmobile');
-    card6.classList.replace('hidemobile', 'showmobile');
-    morebutton.classList.replace('showcontent', 'hidemobile');
-    lessbutton.classList.replace('hidemobile', 'showcontent');
+  card3.classList.replace('hidemobile', 'showmobile');
+  card4.classList.replace('hidemobile', 'showmobile');
+  card5.classList.replace('hidemobile', 'showmobile');
+  card6.classList.replace('hidemobile', 'showmobile');
+  morebutton.classList.replace('showcontent', 'hidemobile');
+  lessbutton.classList.replace('hidemobile', 'showcontent');
 });
 
 lessbutton.addEventListener('click', () => {
-    card3.classList.replace('showmobile', 'hidemobile');
-    card4.classList.replace('showmobile', 'hidemobile');
-    card5.classList.replace('showmobile', 'hidemobile');
-    card6.classList.replace('showmobile', 'hidemobile');
-    morebutton.classList.replace('hidemobile', 'showcontent');
-    lessbutton.classList.replace('showcontent', 'hidemobile');
+  card3.classList.replace('showmobile', 'hidemobile');
+  card4.classList.replace('showmobile', 'hidemobile');
+  card5.classList.replace('showmobile', 'hidemobile');
+  card6.classList.replace('showmobile', 'hidemobile');
+  morebutton.classList.replace('hidemobile', 'showcontent');
+  lessbutton.classList.replace('showcontent', 'hidemobile');
 });
-
-
-
